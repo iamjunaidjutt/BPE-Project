82c3853fd33d56d343f185147044a50867736853
Ahm4dA <ahmadadnan2003aaa@gmail.com>
2023-05-10 13:16:57 +0500

    project initaited

---
+++ src/main/java/com/sdaproject/bookclubpro/BookclubproApplication.java
@@ -0,0 +1,13 @@
+package com.sdaproject.bookclubpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookclubproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookclubproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdaproject/bookclubpro/BookclubproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdaproject.bookclubpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookclubproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
