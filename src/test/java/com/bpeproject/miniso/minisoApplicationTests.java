63ebbfc23cf704c5263c246efb95371fa426caf0
Ahm4dA <ahmadadnan2003aaa@gmail.com>
2023-05-10 13:42:02 +0500

    Everything set to bpeproject and miniso

---
+++ src/test/java/com/bpeproject/miniso/minisoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpeproject.miniso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class minisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
