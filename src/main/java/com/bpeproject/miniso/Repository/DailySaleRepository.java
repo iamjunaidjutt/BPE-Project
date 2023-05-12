3592269e689a90da1bdf4a41d00cf7e856764db7
Shmdtalha <shmdtalha@gmail.com>
2023-05-12 20:30:26 +0500

    Graphs kay bachay

--- src/main/java/com/bpeproject/miniso/Repository/DailySaleRepository.java
+++ src/main/java/com/bpeproject/miniso/Repository/DailySaleRepository.java
@@ -1,6 +1,7 @@
 package com.bpeproject.miniso.Repository;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -18,15 +19,22 @@ public interface DailySaleRepository extends JpaRepository<DailySale, Long>{
 //    List<Object[]> veryBad(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
 
-@Query("SELECT d.date AS date, SUM(d.totalSales) AS totalSales FROM DailySale d WHERE d.date BETWEEN :startDate AND :endDate GROUP BY d.date")
+// Daily Sales
+@Query("SELECT d.date AS date, SUM(d.totalSales) AS totalSales FROM DailySale d WHERE d.date BETWEEN :startDate AND :endDate GROUP BY d.date ORDER BY d.date")
 List<Object[]> findDailySales(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
 // Monthly Sales
-@Query("SELECT FUNCTION('YEAR', d.date) AS year, FUNCTION('MONTH', d.date) AS month, SUM(d.totalSales) AS totalSales FROM DailySale d WHERE d.date BETWEEN :startDate AND :endDate GROUP BY FUNCTION('YEAR', d.date), FUNCTION('MONTH', d.date)")
+@Query("SELECT FUNCTION('YEAR', d.date) AS year, FUNCTION('MONTH', d.date) AS month, SUM(d.totalSales) AS totalSales FROM DailySale d WHERE d.date BETWEEN :startDate AND :endDate GROUP BY FUNCTION('YEAR', d.date), FUNCTION('MONTH', d.date) ORDER BY FUNCTION('YEAR', d.date), FUNCTION('MONTH', d.date)")
 List<Object[]> findMonthlySales(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
 // Yearly Sales
-@Query("SELECT FUNCTION('YEAR', d.date) AS year, SUM(d.totalSales) AS totalSales FROM DailySale d WHERE d.date BETWEEN :startDate AND :endDate GROUP BY FUNCTION('YEAR', d.date)")
+@Query("SELECT FUNCTION('YEAR', d.date) AS year, SUM(d.totalSales) AS totalSales FROM DailySale d WHERE d.date BETWEEN :startDate AND :endDate GROUP BY FUNCTION('YEAR', d.date) ORDER BY FUNCTION('YEAR', d.date)")
 List<Object[]> findYearlySales(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+@Query(value = "SELECT DATEADD(day, (DATEDIFF(day, '2000-01-01', d.date) / 7) * 7, '2000-01-01') AS weekStart,  SUM(d.totalSales) FROM DailySale d WHERE d.date BETWEEN :startDate AND :endDate GROUP BY DATEADD(day, (DATEDIFF(day, '2000-01-01', d.date) / 7) * 7, '2000-01-01') ORDER BY weekStart", nativeQuery = true)
+List<Object[]> findWeeklySales(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+
+Optional<DailySale> findByDate(LocalDate date);
 
 }
--- src/main/java/com/bpeproject/miniso/Service/DailySaleService.java
+++ src/main/java/com/bpeproject/miniso/Service/DailySaleService.java
@@ -1,7 +1,10 @@
 package com.bpeproject.miniso.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +19,15 @@ public class DailySaleService {
     DailySaleRepository dailySaleRepository;
 
     public void addDailySale(DailySale sale) {
-        dailySaleRepository.save(sale);
+
+        Optional<DailySale> entry = dailySaleRepository.findByDate(sale.getDate());
+
+        if (!entry.isPresent())
+            dailySaleRepository.save(sale);
+        else {
+            sale.setId(entry.get().getId());
+            dailySaleRepository.save(sale);
+        }
     }
 
     // public List<Object[]> findDailyStats(LocalDate startDate, LocalDate endDate){
@@ -37,13 +48,20 @@ public class DailySaleService {
         if (interval.equals("daily")) {
             stats = dailySaleRepository.findDailySales(startDate, endDate);
             for (Object[] ob : stats) {
-                System.out.println("Date: " + ob[0] + ", Revenue: " + ob[1]);
+                System.out.println("Date: " + ob[0] + ", Revenue: " + ((Double) ob[1]).intValue());
+            }
+            return stats;
+        } else if (interval.equals("weekly")) {
+            stats = dailySaleRepository.findWeeklySales(startDate, endDate);
+            for (Object[] ob : stats) {
+                System.out.println("Week Start: " + ob[0] + ", Revenue: " + ((Double) ob[1]).intValue());
             }
             return stats;
         } else if (interval.equals("monthly")) {
             stats = dailySaleRepository.findMonthlySales(startDate, endDate);
             for (Object[] ob : stats) {
-                System.out.println("Month: " + ob[0] + ", Year: " + ob[1] + ", Revenue: " + ob[2]);
+                System.out
+                        .println("Month: " + ob[0] + ", Year: " + ob[1] + ", Revenue: " + ((Double) ob[2]).intValue());
             }
             return stats;
         }
@@ -51,38 +69,111 @@ public class DailySaleService {
         else if (interval.equals("yearly")) {
             stats = dailySaleRepository.findYearlySales(startDate, endDate);
             for (Object[] ob : stats) {
-                System.out.println("Year: " + ob[0] + ", Revenue: " + ob[1]);
+                System.out.println("Year: " + ob[0] + ", Revenue: " + ((Double) ob[1]).intValue());
             }
             return stats;
         } else {
             throw new IllegalArgumentException("Invalid interval: " + interval);
         }
 
-        // switch (interval.toLowerCase()) {
-        // case "daily":
-        // stats = dailySaleRepository.findDailySales(startDate, endDate);
-        // for (Object[] ob : stats) {
-        // System.out.println("Date: " + ob[0] + ", Revenue: " + ob[1]);
-        // }
-        // return stats;
+    }
 
-        // case "monthly":
-        // stats = dailySaleRepository.findMonthlySales(startDate, endDate);
-        // for (Object[] ob : stats) {
-        // System.out.println("Month: " + ob[0] + ", Year: " + ob[1] + ", Revenue: " +
-        // ob[2]);
-        // }
-        // return stats;
-        // case "yearly":
-        // stats = dailySaleRepository.findYearlySales(startDate, endDate);
-        // for (Object[] ob : stats) {
-        // System.out.println("Year: " + ob[0] + ", Revenue: " + ob[1]);
-        // }
-        // return stats;
-        // default:
-        // throw new IllegalArgumentException("Invalid interval: " + interval);
-        // }
-        //
+    public List<Integer> pcChanges(List<Object[]> sales) {
+        List<Integer> pcChanges = new ArrayList<Integer>();
+        Double previousSales = null;
+
+        for (Object[] ob : sales) {
+            Double currentSales;
+            try {
+                currentSales = ((Double) ob[2]);
+                // continue processing
+            } catch (ArrayIndexOutOfBoundsException e) {
+                currentSales = ((Double) ob[1]);
+            }
+
+            Double percentageChange = 0.0;
+            if (previousSales != null)
+                percentageChange = ((currentSales - previousSales) / previousSales) * 100;
+
+            previousSales = currentSales;
+            pcChanges.add(percentageChange.intValue());
+        }
+
+        return pcChanges;
+    }
+
+    public List<Integer> pcBaseChanges(List<Object[]> sales) {
+        List<Integer> pcChanges = new ArrayList<Integer>();
+        Double base;
+
+        try {
+            base = ((Double) sales.get(0)[2]);
+            // continue processing
+        } catch (ArrayIndexOutOfBoundsException e) {
+            base = ((Double) sales.get(0)[1]);
+        }
+
+        for (Object[] ob : sales) {
+            Double currentSales;
+
+            try {
+                currentSales = ((Double) ob[2]);
+                // continue processing
+            } catch (ArrayIndexOutOfBoundsException e) {
+                currentSales = ((Double) ob[1]);
+            }
+
+            Double percentageChange = ((currentSales - base) / base) * 100;
+
+            pcChanges.add(percentageChange.intValue());
+        }
+
+        return pcChanges;
+    }
+
+    public void saleGenerator(int numSales) {
+        Random rand = new Random();
+
+        for (int i = 0; i < numSales; i++) {
+            // Generate a random date in the past 2 years
+            long minDay = LocalDate.of(2020, 1, 1).toEpochDay();
+            long maxDay = LocalDate.now().toEpochDay();
+            long randomDay = minDay + rand.nextInt((int) (maxDay - minDay));
+            LocalDate randomDate = LocalDate.ofEpochDay(randomDay);
+
+            // Generate a random total sales value between 100k and 400k
+            double randomTotalSales = 100000 + (400000 - 100000) * rand.nextDouble();
+
+            // Generate random total customers based on sales
+            int randomTotalCustomers;
+            if (randomTotalSales <= 250000) { // Sales are low
+                randomTotalCustomers = 30 + rand.nextInt(51); // 30 to 80
+            } else { // Sales are high
+                randomTotalCustomers = 60 + rand.nextInt(91); // 60 to 150
+            }
+
+            // Generate random total products sold (1-4 times the number of customers)
+            int randomTotalProductsSold = randomTotalCustomers * (1 + rand.nextInt(4));
+
+            // Create and save the new DailySale
+            DailySale dailySale = new DailySale();
+            dailySale.setDate(randomDate);
+            dailySale.setTotalSales(randomTotalSales);
+            dailySale.setTotalProducts(randomTotalProductsSold);
+            dailySale.setNumOfCustomers(randomTotalCustomers);
+
+            double saleswogst = (int) dailySale.getTotalSales() / 1.17;
+            dailySale.setTotalSalesWoGST(saleswogst);
+
+            double avgTval = (int) dailySale.getTotalSales() / dailySale.getNumOfCustomers();
+            dailySale.setAvgTransactionValue(avgTval);
+
+            dailySale.setSalesDay(dailySale.getDate().getDayOfMonth());
+            dailySale.setSalesMonth(dailySale.getDate().getMonthValue());
+            dailySale.setSalesYear(dailySale.getDate().getYear());
+
+            dailySaleRepository.save(dailySale);
+        }
     }
 
 }
